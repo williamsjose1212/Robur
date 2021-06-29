a554faf31d77049c1031fe1b3dd8d8d797814de5
Samipote <SamuelLachance5@gmail.com>
2021-06-29 00:03:21 -0400

    Update SimpleEzreal.lua

--- SimpleEzreal.lua
+++ SimpleEzreal.lua
@@ -310,7 +310,7 @@ function Ezreal.LogicQ()
       local qPred = Ezreal.Q:GetPrediction(enemy)
       local wPred = Ezreal.W:GetPrediction(enemy)
       if not Ezreal.W:IsReady() or not Menu.Get("Combo.W") or (wPred == nil or wPred.HitChanceEnum < HitChanceEnum.Low or wPred.TargetPosition:Distance(wPred.CastPosition) > 80) or not Ezreal.W:CanCast(enemy) then
-        if qPred ~= nil and qPred.HitChanceEnum >= HitChanceEnum.Low and qPred.TargetPosition:Distance(qPred.CastPosition) <= 60 then
+        if qPred ~= nil and qPred.HitChanceEnum >= HitChanceEnum.Medium and qPred.TargetPosition:Distance(qPred.CastPosition) <= 60 then
           if Ezreal.Q:Cast(qPred.CastPosition) then return true end
         end
       end
