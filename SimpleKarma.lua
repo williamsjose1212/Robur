4a6ac15c0291ed32d6c8979815710bf09c93ca1e
Samipote <SamuelLachance5@gmail.com>
2021-07-13 19:08:55 -0400

    Update SimpleKarma.lua

--- SimpleKarma.lua
+++ SimpleKarma.lua
@@ -313,7 +313,7 @@ function Karma.LogicQ()
       local qPred = Karma.Q:GetPrediction(enemy)
       local qPred2 = Karma.Q2:GetPrediction(enemy)
       if not Utils.HasBuff(Player,"KarmaMantra") and (not Karma.R:IsReady() or not Menu.Get("Combo.R"))  then
-        if qPred ~= nil and qPred.HitChanceEnum >= HitChanceEnum.Medium then
+        if qPred ~= nil and qPred.HitChanceEnum >= HitChanceEnum.VeryHigh then
           if Karma.Q:Cast(qPred.CastPosition) then return true end
         end
       elseif Utils.HasBuff(Player,"KarmaMantra") and Utils.CountEnemiesInRange(Player,1200) < 3 then
@@ -402,7 +402,7 @@ end
 function Karma.LogicE()
   if (Combo and Menu.Get("Combo.E") and Player.Mana > eMana+qMana) or (Harass and Menu.Get("Harass.E") and Player.Mana > (eMana + qMana + wMana)*4) then
     for k, enemy in ipairs(Utils.GetTargets(Karma.Q)) do
-      if Menu.Get("1" .. Player.CharName) and Utils.HasBuff(enemy,"KarmaSpiritBind") or (not Utils.HasBuff(Player,"KarmaMantra") or not Karma.Q:IsReady() or Utils.CountEnemiesInRange(Player,1200) >= 3 or Utils.CountHeroes(Player,700,"ally") >= 3) then
+      if Menu.Get("1" .. Player.CharName) and Utils.HasBuff(enemy,"KarmaSpiritBind") or (Utils.HasBuff(Player,"KarmaMantra") and (Utils.CountEnemiesInRange(Player,1200) >= 3 or Utils.CountHeroes(Player,700,"ally") >= 3)) then
         if Karma.E:Cast(Player) then return true end
       end
     end
